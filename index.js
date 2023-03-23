a492fc258321642dc99c22dfc2ce36e8c2706e66
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-03-23 14:28:11 -0700

    added question for inquirer promtps

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 var inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What was your motivation?',
+    'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")',
+    'What problem does it solve?',
+    'What did you learn?',
+    'What makes your project stand out?',
+    'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    'Provide instructions and examples for use. Include screenshots as needed.',
+    'List your collaborators, if any, with links to their GitHub profiles. (If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.)',
+    'The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).',
+    'If your project has a lot of features, list them here.',
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
